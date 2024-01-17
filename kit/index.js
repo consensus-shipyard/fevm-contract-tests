5902670f7a2d420635b191b8867b392ddbd68f55
mike seiler <snissn@gmail.com>
2024-01-16 22:02:01 -1000

    fix url

--- kit/index.js
+++ kit/index.js
@@ -2,7 +2,7 @@ const request = require("sync-request");
 const { ethers } = require("ethers");
 
 // Setup testing environment
-const defaultNodeUrl = "http://127.0.0.1:8545";
+const defaultNodeUrl = "http://127.0.0.1:8545/";
 
 function initNode(filAmount, blockTimeMs) {
     return defaultNodeUrl;
